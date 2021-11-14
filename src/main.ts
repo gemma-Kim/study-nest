aa71a87ab59467222b960be3589611e557278d2b
gemma-Kim <gemmak.rok@gmail.com>
2021-11-14 21:03:50 +0900

    [Add] HttpExceptionFilter 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 
 declare const module: any;
 
@@ -17,6 +18,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(port);
   console.log(`listening on port ${port}`);
