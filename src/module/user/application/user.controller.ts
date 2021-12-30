dc3006421e99b5402447b30bb6b1c2929dedabbc
gemma-Kim <gemmak.rok@gmail.com>
2021-12-30 21:06:19 +0900

    userService exists 추가 및 가입로직 컨트롤러로 이동

--- src/module/user/application/user.controller.ts
+++ src/module/user/application/user.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  NotFoundException,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UndefinedToNullInterceptor } from 'src/common/interceptor/undefinedToNull.interceptor';
 import { JoinRequestDto } from '../dto/join.request.dto';
@@ -7,6 +14,8 @@ import { Email, Nickname, Password, User } from '../domain/user.domain';
 import { JoinResponseDto } from '../dto/user.response.dto';
 import { LogInRequestDto } from '../dto/user.request.dto';
 import { AuthService } from 'src/module/auth/application/auth.service';
+import { UserRepository } from '../entity/user.reposiory';
+import { AuthPayload } from 'src/module/auth/dto/auth.payload.dto';
 
 @UseInterceptors(UndefinedToNullInterceptor)
 @ApiTags('user')
@@ -15,6 +24,7 @@ export class UserController {
   constructor(
     private readonly userService: UserService,
     private readonly authService: AuthService,
+    private readonly userRepository: UserRepository,
   ) {}
 
   @ApiOperation({
@@ -22,18 +32,55 @@ export class UserController {
   })
   @Post('signUp')
   async signUp(@Body() data: JoinRequestDto): Promise<JoinResponseDto> {
-    const email = new Email(data.email);
-    const password = new Password(data.password);
-    const nickname = new Nickname(data.nickname);
-    const user = new User(email, password, nickname);
-    return await this.userService.signUp(user);
+    const user = new User(
+      new Email(data.email),
+      new Password(data.password),
+      new Nickname(data.nickname),
+    );
+    const foundUser = await this.userService.exists(user);
+    if (foundUser) {
+      throw new BadRequestException('이미 존재하는 사용자 이메일 혹은 닉네임');
+    }
+
+    const hashedPassword = this.authService.hashPassword(
+      new Password(user.password),
+    );
+
+    const signUpedNewUser = await this.userRepository.saveUser({
+      email: user.email,
+      password: hashedPassword,
+      nickname: user.nickname,
+    });
+
+    return new JoinResponseDto(signUpedNewUser);
   }
 
   @Post('login')
   async logIn(@Body() data: LogInRequestDto) {
-    const email = new Email(data.email);
-    const password = new Password(data.password);
+    const userData: User = await this.userRepository.findOne({
+      email: data.email,
+    });
 
-    return await this.authService.validateUser(email, password);
+    if (!userData) {
+      throw new NotFoundException();
+    }
+
+    const user = new User(
+      new Email(userData.email),
+      new Password(userData.password),
+      new Nickname(userData.nickname),
+    );
+
+    await this.authService.validatePassword(
+      new Password(data.password),
+      new Password(user.password),
+    );
+
+    return this.authService.generateAccessToken(
+      new AuthPayload({
+        id: user.id,
+        email: user.email,
+      }),
+    );
   }
 }
--- src/module/user/application/user.service.ts
+++ src/module/user/application/user.service.ts
@@ -1,11 +1,9 @@
-import * as bcrypt from 'bcrypt';
-
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from '../entity/user.reposiory';
 import { JoinResponseDto } from '../dto/user.response.dto';
-import { User } from '../domain/user.domain';
-import { NotFoundError } from 'rxjs';
+import { Nickname, Password, User } from '../domain/user.domain';
+import { AuthService } from 'src/module/auth/application/auth.service';
 
 @Injectable()
 export class UserService {
@@ -14,29 +12,9 @@ export class UserService {
     private readonly userRepository: UserRepository,
   ) {}
 
-  async findOne(email: string) {
-    return this.userRepository.findOne({ where: { email } });
-  }
-
-  async exist(user: User) {
-    const foundUser = this.userRepository.findOne({
-      where: { email: user.email },
+  async exists(user: User) {
+    return this.userRepository.findOne({
+      where: user,
     });
-
-    if (!foundUser) {
-      throw new NotFoundException('NOT_FOUND_USER');
-    }
-  }
-
-  async signUp(userData: User) {
-    const saltRounds: number = Number(process.env.SALT_ROUNDS);
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hashedPassword = bcrypt.hash(userData.password, salt);
-    const signUpedNewUser = await this.userRepository.saveUser({
-      email: userData.email,
-      password: hashedPassword,
-      nickname: userData.nickname,
-    });
-    return new JoinResponseDto(signUpedNewUser);
   }
 }
