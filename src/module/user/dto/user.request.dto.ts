e574f5d5d4d7be80564cda63058156b21774e894
gemma-Kim <gemmak.rok@gmail.com>
2022-01-01 21:51:49 +0900

    update user info request dto 이동

--- src/module/user/dto/user.request.dto.ts
+++ src/module/user/dto/user.request.dto.ts
@@ -18,3 +18,21 @@ export class LogInRequestDto {
   @IsString()
   public readonly password: string;
 }
+
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdateUserInfo {
+  @ApiProperty({
+    example: 'email123@naver.com',
+    description: '이메일',
+    required: false,
+  })
+  public email: string;
+
+  @ApiProperty({
+    example: '12345678',
+    description: '비밀번호',
+    required: false,
+  })
+  public password: string;
+}
