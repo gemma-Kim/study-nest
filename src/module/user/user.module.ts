5a0f76941709ee7b29d91f88d71c9d78477cff4f
gemma-Kim <gemmak.rok@gmail.com>
2022-01-01 21:55:07 +0900

    AuthService 추가

--- src/module/user/user.module.ts
+++ src/module/user/user.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { UserService } from './application/user.service';
+import { UserController } from './application/user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './entity/user.reposiory';
 import { ProfileRepository } from '../profile/repository/profile.repository';
+import { AuthService } from '../auth/application/auth.service';
 
 @Module({
   exports: [UserService],
   imports: [TypeOrmModule.forFeature([UserRepository, ProfileRepository])],
-  providers: [UserService],
+  providers: [UserService, AuthService],
   controllers: [UserController],
 })
 export class UserModule {}
