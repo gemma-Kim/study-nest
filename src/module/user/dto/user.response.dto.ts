97a8b8928c36e091718f76b4084b441d40f7efe2
gemma-Kim <gemmak.rok@gmail.com>
2022-01-08 00:11:05 +0900

    UpdateUserInfoResponseDto 추가

--- src/module/user/dto/user.response.dto.ts
+++ src/module/user/dto/user.response.dto.ts
@@ -1,12 +1,25 @@
 export class JoinResponseDto {
-  id: number;
+  userId: number;
   email: string;
   nickname: string;
   createAt: string;
   constructor(data) {
-    this.id = data.id;
+    this.userId = data.id;
     this.email = data.email;
     this.nickname = data.nickname;
     this.createAt = data.createAt;
+  }
+}
+
+export class updateUserInfoResponseDto {
+  userId: number;
+  email: string;
+  nickname: string;
+  updateAt: string;
+  constructor(data) {
+    this.userId = data.id;
+    this.email = data.email;
+    this.nickname = data.nickname;
+    this.updateAt = data.updateAt;
   }
 }
