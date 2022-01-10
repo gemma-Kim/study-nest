b4054bf59062817c1041a76d92ae4884fde5c185
gemma-Kim <gemmak.rok@gmail.com>
2022-01-11 00:13:26 +0900

    user module domain service 추가

--- src/module/user/domain/user.service.ts
+++ src/module/user/domain/user.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entity/user.entity';
 import { IUserRepository, UserRepository } from '../repository/user.reposiory';
-import { HashedPassword, Password } from './user.domain';
-import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -12,7 +10,11 @@ export class UserService {
     private readonly userRepository: IUserRepository,
   ) {}
 
-  async exists(userData): Promise<User> {
+  async exists(userData: {
+    id?: number;
+    email?: string;
+    nickname?: string;
+  }): Promise<User> {
     return await this.userRepository.Find({
       where: [
         { id: userData.id },
@@ -20,11 +22,5 @@ export class UserService {
         { nickname: userData.nickname },
       ],
     });
-  }
-
-  async hashPassword(password: Password): Promise<string> {
-    const salt = await bcrypt.genSalt(Number(process.env.SALT_ROUNDS));
-    const pwValue = password.value;
-    return new HashedPassword(await bcrypt.hash(pwValue, salt)).value;
   }
 }
