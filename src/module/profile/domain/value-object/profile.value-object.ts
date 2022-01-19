6868534c137edcfa1abdb98f3449041627e642fa
gemma-Kim <gemmak.rok@gmail.com>
2022-01-20 00:32:23 +0900

    [Add] profile value-object 추가

--- src/module/profile/domain/value-object/profile.value-object.ts
+++ src/module/profile/domain/value-object/profile.value-object.ts
@@ -2,7 +2,7 @@ import { BadRequestException } from '@nestjs/common';
 import { genderType, genderTypeList } from '../../type/profile.type';
 
 export class Gender {
-  constructor(readonly value: string) {
+  constructor(readonly value: genderType) {
     if (value && !genderTypeList.includes(value)) {
       throw new BadRequestException('INVALID_GENDER_TYPE');
     }
@@ -12,17 +12,5 @@ export class Gender {
 export class Photo {
   constructor(readonly value: string) {
     this.value = value;
-  }
-}
-
-export class UserProfile {
-  constructor(
-    readonly id: number,
-    readonly gender: genderType,
-    readonly photo: string,
-  ) {
-    this.id = id;
-    this.gender = new Gender(gender).value;
-    this.photo = new Photo(gender).value;
   }
 }
